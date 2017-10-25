4265569cb810f897f00d68ee9cdd3d4b1b12a552
Andrew Morton <morton09@me.com>
2017-10-25 12:02:41 -0500

    mongo connected

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
-
 const express = require('express');
 const bodyParser = require('body-parser')
 const hbs = require('express-handlebars');
-const app     = express();
+const app = express();
+const dbConfig = require('./mongo/dbconfig');
+const mongoose = require('mongoose');
+
 
 app.engine('handlebars', hbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.listen('8080');
 app.disable("x-powered-by");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('views/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/public', express.static(__dirname + "/views/public"));
+mongoose.connect(dbConfig.url);
 
 console.log("hello its me i think im self aware");
 console.log("I Believe to be running on port 8080");
 
+
+
 app.get('/', (req, res) => {
+    res.render('home')
+});
 
 
 
-        res.render('home')
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
---
+++ mongo/dbconfig.js
@@ -0,0 +1,3 @@
+module.exports = {
+    'url' : 'mongodb://light:Light123@ds231245.mlab.com:31245/heroku_tngpmb0s'
+    }
